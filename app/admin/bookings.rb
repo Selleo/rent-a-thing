b7b4df2811caa804aba20e58f1f8d87579ad9645
Błażej Kosmowski <b.kosmowski@selleo.com>
2021-08-23 12:36:47 +0200

    [WIP] / [TODROP] Testing views

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -9,6 +9,44 @@ ActiveAdmin.register Booking do
   filter :created_at
 
   # ==============
+  # ==== SHOW ====
+  # ==============
+
+  show do
+    columns do
+      column do
+        attributes_table do
+          row :starts_on
+          row :ends_on
+        end
+      end
+
+      column do
+        attributes_table do
+          row :customer
+        end
+
+        active_admin_comments
+      end
+    end
+
+    columns do
+      column do
+        panel 'Items' do
+          header_action link_to('All items', [:admin, :items])
+
+          table_for booking.booked_items do |booked_item|
+            column :item
+            column :description do |booked_item|
+              booked_item.item.description
+            end
+          end
+        end
+      end
+    end
+  end
+
+  # ==============
   # ==== EDIT ====
   # ==============
 
--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -12,4 +12,18 @@ ActiveAdmin.register Customer do
   # ==============
 
   permit_params :full_name, :email, :phone
+
+
+  form do |f|
+    inputs 'Basic info', class: 'two-column' do
+      input :full_name
+    end
+
+    inputs 'Contact info', class: 'two-column' do
+      input :email
+      input :phone
+    end
+
+    actions
+  end
 end
