8a882e96e17e8bae74e4478b0647212c76462063
Konrad Pazgan <hello@konradpazgan.pl>
2021-08-25 19:45:13 +0200

    Add bookings list to customer page

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -18,6 +18,35 @@ ActiveAdmin.register Customer do
   end
 
   # ==============
+  # ==== SHOW ====
+  # ==============
+
+  show do
+    columns do
+      column do
+        attributes_table do
+          row :full_name
+          row :email
+          row :phone
+        end
+      end
+
+      column do
+        panel 'Bookings' do
+          table_for resource.bookings do
+            column :id do |booking|
+              @id = booking.id
+              render partial: 'id_link', locals: {id: booking.id}
+            end
+            column :starts_on
+            column :ends_on
+          end
+        end
+      end
+    end
+  end
+
+  # ==============
   # ==== EDIT ====
   # ==============
 
