27527e79112f2035c1429d3098a443471de7ae3e
Błażej Kosmowski <b.kosmowski@selleo.com>
2021-08-25 17:24:14 +0200

    Add categories model

---
+++ app/admin/categories.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register Category do
+  menu priority: 5
+
+  # ==============
+  # ==== FORM ====
+  # ==============
+
+  permit_params :name, :description
+
+  # ==============
+  # ==== SHOW ====
+  # ==============
+
+  show do
+    columns do
+      column do
+        attributes_table do
+          row :name
+          row :description
+        end
+
+        active_admin_comments
+      end
+
+      column do
+        panel 'Items' do
+          table_for resource.items do
+            column :name
+            column :description
+          end
+        end
+      end
+    end
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :booked_items
   has_many :bookings, through: :booked_items, dependent: :destroy
+  belongs_to :category
 
   scope :active, -> { where(archived: false) }
   scope :archived, -> { where(archived: true) }
