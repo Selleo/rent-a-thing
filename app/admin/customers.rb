37980a9f173a197d88051d74532f0aab31fce310
Filip Kania <fkania2@gmail.com>
2021-08-26 13:52:45 +0200

    refactor: change layout of customer view

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -21,6 +21,31 @@ ActiveAdmin.register Customer do
     actions
   end
 
+  show do
+    columns do
+      column do
+        attributes_table do
+          row :full_name
+          row :email
+          row :phone
+        end
+
+        active_admin_comments
+      end
+
+      column do
+        panel "Bookings" do
+          table_for customer.bookings do
+            column("Booking") { |booking| booking }
+            column("Items") { |booking| booking.booked_items.map { |booked_item| booked_item.item.name } }
+            column :starts_on
+            column :ends_on
+          end
+        end
+      end
+    end
+  end
+
   # ==============
   # ==== EDIT ====
   # ==============
