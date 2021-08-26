e6d9422d4d091988009f39de810f6c6289584f29
snacksncode <snacksncode@gmail.com>
2021-08-26 19:32:33 +0200

    Implemented UUID for Bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,10 @@
 class Booking < ApplicationRecord
+  self.primary_key = :uuid
   belongs_to :customer
   has_many :booked_items, dependent: :destroy
   has_many :items, through: :booked_items
+
+
 
   scope :available, -> { where(archived_at: nil).where("ends_on > :date", date: Time.current) } 
 
