0bc2ba119bed723d9e164a1354b4b5bfc8217d29
Paweł Biegun <biegunpawel900@gmail.com>
2021-08-26 18:34:35 +0200

    booking archive

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,4 +6,11 @@ class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
   end
+
+  def destroy
+    booking = Booking.find(params[:id])
+    booking.archived_at = Time.current
+    booking.save
+    redirect_to bookings_url, notice: "Booking has been archived"
+  end
 end
---
+++ db/migrate/20210826160448_add_archive_at_to_booking.rb
@@ -0,0 +1,5 @@
+class AddArchiveAtToBooking < ActiveRecord::Migration[6.1]
+  def change
+    add_column :bookings, :archived_at, :datetime
+  end
+end
