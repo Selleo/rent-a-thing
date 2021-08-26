aa765d5b65ecf7b74808b3a230ac79bb2286b409
Rasssa <mateusz.pilarski996@gmail.com>
2021-08-26 18:31:50 +0200

    Zrobienie zadania 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,12 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+=begin
+  def destroy
+    @booking_delete = Booking.find(params[:id]).destroy
+  end
+=end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_161025) do
+ActiveRecord::Schema.define(version: 2021_08_25_201425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,9 +76,9 @@ ActiveRecord::Schema.define(version: 2021_08_25_161025) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "description"
-    t.boolean "archived", default: false
+    t.boolean "archived"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
