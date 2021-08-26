3184899f512116cfc18d843582deb3fa62ca1473
Robert Kubica <robert.kubica1982@gmail.com>
2021-08-26 19:32:59 +0200

    26-08-2021

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,21 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def show
+    # if Bookings(archived_at) !== nil
+    # render '404'
+    # else
+      @booking = Booking.find(params[:id])
+    # end
+  end
+
+  def destroy
+    booking = Booking.find(params[:id])
+    booking.update(archived_at: Time.current)
+    redirect_to bookings_path, notice: "Booking has been archived successfully"
+  end
+
+
+
 end
