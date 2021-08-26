3ccccc41c17ab6bcf827f2e8afbae294233caab5
Filip Kania <fkania2@gmail.com>
2021-08-26 12:43:49 +0200

    fix: adds validation for models

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,7 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 64 }
+  validates :encrypted_password, presence: true
 end
