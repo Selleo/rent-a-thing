7178628ee44cb54f4feee77b60e0c99a37348c80
smoczis <romanowski.mateusz93@gmail.com>
2021-08-26 19:32:00 +0200

    Not finished delete controller method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,12 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.archived_at = Time.current
+    # redirect_to index
+  end
 end
