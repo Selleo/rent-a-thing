b511409f775721271b293ee2513bcf2e0d016ef1
wblazej <blazej.wrzosok@gmail.com>
2021-08-26 18:19:42 +0200

    Bookings listing, showing and archiving

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,21 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def show
+    @booking = Booking.find_by_id(params[:id])
+
+    if @booking.archived_at || @booking.ends_on < Time.current
+      render "bookings/404"
+    end
+
+
+  end
+
+  def destroy
+    booking = Booking.find_by_id(params[:id])
+    booking.archived_at = Time.current
+    booking.save
+    redirect_to bookings_path, notice: "Booking has been archived successfully"
+  end
 end
