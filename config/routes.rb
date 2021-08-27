58da2103e56fac939615546c43d60ad2e0dd93e5
Błażej Kosmowski <b.kosmowski@selleo.com>
2021-08-27 19:29:28 +0200

    Booked days by month

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     get 'available_items' => 'available_items#index'
+    get 'booked_days_by_month' => 'booked_days_by_month#show'
   end
 end
