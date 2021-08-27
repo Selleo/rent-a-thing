ec3b0ac50785f3dc795641c0ecb3b5f8e78d723e
smoczis <romanowski.mateusz93@gmail.com>
2021-08-27 18:50:22 +0200

    Show montsh in JSON responnse"

--- app/controllers/v1/booked_days_controller.rb
+++ app/controllers/v1/booked_days_controller.rb
@@ -2,14 +2,21 @@ module V1
   class BookedDaysController < ActionController::API
     def index
 
-      # returns list of all months in current year
+      # returns list of all months in current year till now
 
-      months = Date.current.year.months
+      months = (Date.parse("2021-01-01")..Date.current).to_a.group_by { |date_str| date_str.month }.keys
+
+      result = months.map { |month| { "month": Date.current.year.to_s + sprintf("-%02d", month) } }
+
+      # create hash with structure:
+      # [
+      #   {"month": "2021-01", "values": 0 by default}
+      # ]
+
       # each with a total number of days that were booked in given month
       # as json
 
-      render json: months.as_json
-
+      render json: result.as_json
 
     end
   end
