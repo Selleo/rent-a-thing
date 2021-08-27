673793ffc081f21ef5e5e9e838cf881925ae96d9
Błażej Kosmowski <b.kosmowski@selleo.com>
2021-08-27 17:42:30 +0200

    Available Items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :bookings, only: :index
+
+  namespace :v1 do
+    get 'available_items' => 'available_items#index'
+  end
 end
