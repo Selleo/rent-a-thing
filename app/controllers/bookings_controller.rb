802f5b5b8a94e0e6e792f31d5ada922f505ad058
Irek Skrobis <irek.skrobis@gmail.com>
2021-08-27 11:22:26 +0200

    bookings_controller show 404 if bookings is not active

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,12 @@
 class BookingsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :show_404
+
   def index
     @bookings = Booking.all
   end
 
   def show
-    @booking = Booking.find(params[:id])
+    @booking = Booking.active.find(params[:id])
   end
 
   def destroy
@@ -12,5 +14,9 @@ class BookingsController < ApplicationController
     @booking.update(archived_at: Time.current)
 
     redirect_to bookings_path, notice: 'Booking has been archived successfully.'
+  end
+
+  def show_404
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
   has_many :items, through: :booked_items
 
   accepts_nested_attributes_for :booked_items, allow_destroy: true
+
+  scope :active, -> { where(archived_at: nil).where("ends_on > :ends_on", ends_on: Time.current) }
 end
