d2fbb7183bf506452c19ae95a434e20c37b0269f
Filip Kania <fkania2@gmail.com>
2021-08-30 17:58:37 +0200

    feat: added users per month chart and added randomizing created_at when new customer is created

--- app/controllers/v1/users_per_month_controller.rb
+++ app/controllers/v1/users_per_month_controller.rb
@@ -1,19 +1,21 @@
-class V1::NewUsersPerMonthController < ActionController::API
+class V1::UsersPerMonthController < ActionController::API
   def show
     all_customers = Customer.all
 
     first_user_created = Customer.first.created_at
+    c = 0
     result = (first_user_created.year..Date.current.year).map do |year|
       (1..[Date.current.month, 12].min).map { |month|
         date = Date.new(year, month)
-        { category: date.strftime("%Y-%m"), value: all_customers.where(created_at: date.all_month).count }
+        c += all_customers.where(created_at: date.all_month).count
+        { category: date.strftime("%Y-%m"), value: c }
       }
     end.flatten
 
     render json: {
       data: {
         attributes: {
-          name: 'New users',
+          name: 'Users',
           value: result
         }
       }
