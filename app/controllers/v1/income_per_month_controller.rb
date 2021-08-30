46553e7f795bbbc84b7557febcccb0807c37caad
Filip Kania <fkania2@gmail.com>
2021-08-30 18:42:50 +0200

    fix: fixed wrong price per day calculation

--- app/controllers/v1/income_per_month_controller.rb
+++ app/controllers/v1/income_per_month_controller.rb
@@ -5,16 +5,18 @@ module V1
 
       first_booking_created = Booking.first.created_at
       result = (first_booking_created.year..Date.current.year).flat_map do |year|
-        (1..[Date.current.month, 12].min).flat_map { |month|
-          date = Date.new(year, month)
-          income = all_bookings.where(created_at: date.all_month).sum { |booking|
+        (1..[Date.current.month, 12].min).flat_map do |month|
+          this_month = Date.new(year, month)
+
+          income = all_bookings.where(created_at: this_month.all_month).sum do |booking|
             price_per_day = booking.items.sum(&:price_per_day)
-            days = [1, (booking.ends_on - booking.starts_on).to_i].max
+            days = [1, (booking.ends_on - booking.starts_on).to_i + 1].max
 
             price_per_day * days
-          }
-          { category: date.strftime("%Y-%m"), value: income.to_f }
-        }
+          end
+
+          { category: date.strftime('%Y-%m'), value: income.to_f }
+        end
       end
 
       render json: {
