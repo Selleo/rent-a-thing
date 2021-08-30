25cb640168ab1dac4304438b6e64101034ded97a
wblazej <blazej.wrzosok@gmail.com>
2021-08-30 18:26:36 +0200

    Task 2 30.08

--- app/controllers/v1/new_customers_by_month_controller.rb
+++ app/controllers/v1/new_customers_by_month_controller.rb
@@ -1,28 +1,21 @@
 module V1
   class NewCustomersByMonthController < ActionController::API
-    def show
-      new_customer_by_month = Customer.
-        order(:created_at).
-        group_by { |customer| customer.created_at.strftime('%b %Y') }.
-        transform_values(&:count)
-
-      month = Customer.minimum(:created_at).beginning_of_month
-      result = []
-
-      while month < Date.current
-        month_formatted = month.strftime('%b %Y')
-        result << {
-          category: month_formatted,
-          value: new_customer_by_month.fetch(month_formatted, 0)
+    def index
+      date_range = (Date.new(Customer.minimum(:created_at).year, Customer.minimum(:created_at).month)..Date.current)
+      date_months = date_range.map {|d| Date.new(d.year, d.month, 1) }.uniq
+      customers = date_months.map do |month|
+        month = month.strftime("%Y-%m")
+        {
+          category: month,
+          value: Customer.created_in_month(month).count
         }
-        month = month + 1.month
       end
 
       render json: {
         data: {
           attributes: {
             name: 'New customers by month',
-            value: result
+            value: customers
           }
         }
       }
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ApplicationRecord
   has_many :bookings, dependent: :destroy
+
+  scope :created_in_month, -> (given_month) {
+    where("created_at >= :beginning_of_month and created_at <= :end_of_month",
+          beginning_of_month: Date.parse(given_month + "-01").beginning_of_month,
+          end_of_month: Date.parse(given_month + "-01").end_of_month)
+  }
 end
