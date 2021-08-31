ce393a0640c1b36c969b02867e59c30de9e7fff6
Filip Kania <fkania2@gmail.com>
2021-08-31 12:49:03 +0200

    refactor: changed bookings by item controller

--- app/controllers/v1/bookings_by_item_controller.rb
+++ app/controllers/v1/bookings_by_item_controller.rb
@@ -1,14 +1,14 @@
 module V1
   class BookingsByItemController < ActionController::API
     def show
-      result = Item.all.filter { |item| item.bookings.count.positive? }.map { |item|
+      result = Item.all.filter { |item| item.bookings.count.positive? }.map do |item|
         { category: item.name, value: item.bookings.count }
-      }
+      end
 
       render json: {
         data: {
           attributes: {
-            name: 'Bookings by item',
+            name: 'Bookings by Item',
             value: result
           }
         }
