cb8f26414197fde6d198318ee30b579127e89274
Filip Kania <fkania2@gmail.com>
2021-08-31 12:47:44 +0200

    refactor: changed users per month controller

--- app/controllers/v1/users_per_month_controller.rb
+++ app/controllers/v1/users_per_month_controller.rb
@@ -1,24 +1,23 @@
-class V1::UsersPerMonthController < ActionController::API
-  def show
-    all_customers = Customer.all
+module V1
+  class UsersPerMonthController < ActionController::API
+    include Helpers
 
-    first_user_created = Customer.first.created_at
-    c = 0
-    result = (first_user_created.year..Date.current.year).map do |year|
-      (1..[Date.current.month, 12].min).map { |month|
-        date = Date.new(year, month)
-        c += all_customers.where(created_at: date.all_month).count
-        { category: date.strftime("%Y-%m"), value: c }
-      }
-    end.flatten
+    def show
+      customer_count = 0
+      result = generate_months(Customer.first_created.created_at).map do |month|
+        customer_count += Customer.in_month(month).count
 
-    render json: {
-      data: {
-        attributes: {
-          name: 'Users',
-          value: result
+        { category: month.strftime('%Y-%m'), value: customer_count }
+      end
+
+      render json: {
+        data: {
+          attributes: {
+            name: 'Users per Month',
+            value: result
+          }
         }
       }
-    }
+    end
   end
 end
