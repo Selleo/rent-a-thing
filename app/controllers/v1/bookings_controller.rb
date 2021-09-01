9dd5bd3588acde0878894e3425708c4620ab590b
Filip Kania <fkania2@gmail.com>
2021-09-01 19:26:44 +0200

    refactor: changed code in the bookings controller

--- app/controllers/v1/bookings_controller.rb
+++ app/controllers/v1/bookings_controller.rb
@@ -1,47 +1,63 @@
 class V1::BookingsController < ActionController::API
   def create
-    if params.require(:item_ids)
-      return render html: "empty_item_ids", status: :bad_request
+    if booking_is_in_the_past? || items_not_passed? || customer_attributes_blank?
+      return render json: {}, status: :bad_request
     end
 
-    customer = Customer.find_or_create_by!(
-      full_name: params[:customer_name],
-      email: params[:customer_email],
-      phone: params[:customer_phone]
-    )
+    if booking.new_record?
+      return render json: {}, status: :conflict if item_overlaps_booking?
 
-    booking = customer.bookings.find_by(
-      starts_on: params[:starts_on].to_date,
-      ends_on: params[:ends_on].to_date,
-      archived_at: nil
-    )
-
-    unless booking.present?
-      booking = customer.bookings.create!(
-        starts_on: params[:starts_on].to_date,
-        ends_on: params[:ends_on].to_date
-      )
-
-      params[:item_ids].each do |item_id|
-        item = Item.find(item_id)
-
-        if item.bookings.where(starts_on: booking.starts_on..booking.ends_on).present? ||
-          item.bookings.where(ends_on: booking.starts_on..booking.ends_on).present?
-
-          booking.destroy!
-          return render html: 'item_conflict', status: 409
-        end
-
-        booking.items << Item.find(item_id)
-      end
+      booking.items = items
+      booking.save!
     end
 
     render json: {
       customer_name: customer.full_name,
       customer_phone: customer.phone,
       booking_id: booking.id,
-      booked_items: booking.booked_items.map { |booked_item| booked_item.item.name }.join(', ')
-    }.merge(booking.as_json(only: %i[starts_on ends_on])), status: :created
+      booked_items: booking.items.map(&:name).join(', '),
+      starts_on: booking.starts_on,
+      ends_on: booking.ends_on
+    }, status: :created
+  end
 
+  private
+
+  def customer
+    @customer ||= Customer.where(email: params[:customer_email]).first_or_create(
+      full_name: params[:customer_name],
+      phone: params[:customer_phone]
+    )
+  end
+
+  def booking
+    @booking ||= customer.bookings.where(
+      starts_on: params[:starts_on].to_date,
+      ends_on: params[:ends_on].to_date
+    ).first_or_initialize
+  end
+
+  def items
+    @items ||= Item.where(id: params[:item_ids])
+  end
+
+  def booking_is_in_the_past?
+    params[:starts_on].to_date < Date.current || params[:ends_on].to_date < params[:starts_on].to_date
+  end
+
+  def items_not_passed?
+    params[:item_ids].any?(&:blank?)
+  end
+
+  def customer_attributes_blank?
+    params[:customer_name].blank? || params[:customer_phone].blank?
+  end
+
+  def item_overlaps_booking?
+    new_booking_range = booking.starts_on..booking.ends_on
+
+    items.flat_map(&:bookings).any? do |booking|
+      new_booking_range.overlaps?(booking.starts_on..booking.ends_on)
+    end
   end
 end
