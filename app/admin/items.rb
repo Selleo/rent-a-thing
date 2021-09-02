59d7c23eefad522deca6625845fca3ef8fc9db48
JustynaKedzierska <jus.kedzierska@gmail.com>
2021-09-02 21:06:39 +0200

    add changes 2.09

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,6 +1,13 @@
 ActiveAdmin.register Item do
   menu label: 'Inventory', priority: 4
 
+  member_action :delete_item_image, method: :delete do
+    attachment = ActiveStorage::Attachment.find(params[:id])
+    attachment.destroy!
+    #redirect_to resource_path, notice: "Image deleted"
+    redirect_back(fallback_location: "/")
+  end
+
   # ==============
   # ==== LIST ====
   # ==============
@@ -15,7 +22,7 @@ ActiveAdmin.register Item do
   # ==== FORM ====
   # ==============
 
-  permit_params :name, :description, :category_id, :archived, :fotos
+  permit_params :name, :description, :category_id, :archived, fotos: []
 
   form do |f|
     inputs do
@@ -23,9 +30,8 @@ ActiveAdmin.register Item do
       input :description
       input :category
       input :archived
-      input :fotos, as: :file
+      input :fotos, as: :file, input_html: { multiple: true }
     end
-
     actions
   end
 
@@ -33,10 +39,45 @@ ActiveAdmin.register Item do
   controller do
     def create
       @item = Item.new
-      binding.pry
-      @item.booked_items = params[:item][]
+      @item.name = params[:item][:name]
+      @item.description = params[:item][:description]
+      @item.category_id = params[:item][:category_id]
+      @item.archived = params[:item][:archived]
+      @item.fotos.attach(params[:item][:fotos]) 
+      @item.save
       
-  
+      redirect_back(fallback_location: "/")
+
     end    
   end
+
+  # ==============
+  # ==== SHOW ====
+  # ==============
+
+  show do
+    columns do
+      column do
+        attributes_table do
+          row :name
+          row :description
+          row :archived
+          row :created_at
+          row :updated_at
+          row :price_per_day
+          row :fotos do
+            item.fotos.map do |foto|
+              li do 
+                image_tag(url_for(foto)) if foto.present?
+                div do
+                  link_to "Delete_foto", delete_item_image_admin_item_path(foto), method: :delete
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   has_many :booked_items
   has_many :bookings, through: :booked_items, dependent: :destroy
   belongs_to :category
+  has_many_attached :fotos
 
   scope :active, -> { where(archived: false) }
   scope :archived, -> { where(archived: true) }
